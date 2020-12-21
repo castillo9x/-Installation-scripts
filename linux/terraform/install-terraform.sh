af241aed6f103bbf3c8fc37c9296951bf20cba0c
Randolfo Castillo <castillo.randolfo2@gmail.com>
2020-12-21 16:36:31 -0300

    Fix: Check command and add snap command

--- linux/terraform/install-terraform.sh
+++ linux/terraform/install-terraform.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo "Check curl"
+if ! command -v curl &> /dev/null
+then
+    echo "ERROR: curl could not be found"
+    exit
+fi
+
 # Install Terraform
-echo "Add the HashiCorp GPG key."
+echo "|*** Add the HashiCorp GPG key."
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-echo -n "Add the official HashiCorp Linux repository."
+echo "|*** Add the official HashiCorp Linux repository."
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-echo -n "Update and install."
-sudo apt-get update -y && sudo apt-get install terraform -y
-echo -n "Verify the installation"
-terraform -help
+echo "|*** Update,install and verify"
+sudo sudo snap install terraform && terraform -help
+echo "|*** Verify the installation"
+
